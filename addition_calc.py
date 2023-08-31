c45e522117becbf04726136be8b84d8c202a7a41
James Kim <jkim9312@gmail.com>
2023-08-31 16:40:23 -0400

    test commit to main branch?

---
+++ addition_calc.py
@@ -0,0 +1,8 @@
+def addNums(num1, num2):
+
+
+    return(num1+num2)
+
+num1 = float(input('Please enter the first number you would like to add'))
+num2 = float(input('Please enter the second number you would like to add'))
+print(f"The sum of both numbers is {addNums(num1,num2)}")
